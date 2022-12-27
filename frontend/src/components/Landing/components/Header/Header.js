1eb2276684e1866d11d1345c2549a1a62e19d6b0
Abhishek93248 <abhishek93248@gmail.com>
2022-12-27 10:28:51 +0530

    final

---
+++ frontend/src/components/Landing/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import header from '../../assets/images/header-food.png'
+import Button from '../Button/Button'
+import './Header.css'
+import {useHistory} from 'react-router-dom'
+const Header = () => {
+  const history=useHistory();
+  const handleClick=()=>{
+    history.push("/home");
+  }
+  
+  return (
+    <div className='header' id='home'>
+        <img className='header__image' src={header} />
+        <div className='header__content'>
+            <h1>EAT<br/>HEALTHY<br/>BE HEALTHY</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
+            <Button text='Order Now' onClick={handleClick} ></Button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
